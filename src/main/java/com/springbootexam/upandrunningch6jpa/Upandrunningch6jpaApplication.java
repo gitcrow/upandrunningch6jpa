41afd9d2b0b096687698504adcccdc9a7c1e4ec5
최영민 <justdoithaha@gmail.com>
2024-10-24 17:17:14 +0900

    ch6jpa start

---
+++ src/main/java/com/springbootexam/upandrunningch6jpa/Upandrunningch6jpaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootexam.upandrunningch6jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upandrunningch6jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Upandrunningch6jpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootexam/upandrunningch6jpa/Upandrunningch6jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootexam.upandrunningch6jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Upandrunningch6jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
