3753e4b97327a5912665144cfb801f0532f617ca
최영민 <justdoithaha@gmail.com>
2024-10-25 17:21:26 +0900

    update

---
+++ src/main/java/com/springbootexam/upandrunningch6jpa/model/AircraftRepository.java
@@ -0,0 +1,7 @@
+package com.springbootexam.upandrunningch6jpa.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long>{
+    
+}
